7662e02ae77a64f157b1325dbe87c70e0e13491c
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-06 19:57:40 +0300

    Asp.Net Mvc -Jquery.Get ve Jquery.Get ile Veri Gönderme

---
+++ AspNetMvcJQueryAjax/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcJQueryAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<string> listem = new List<string>() { "turkiye", "cumhuriyeti", "merkez", "bankasi" };
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public PartialViewResult VerileriGetir(string veri = "")
+
+        {
+            if (string.IsNullOrEmpty(veri)==false)   //bosluk yada bos degilse
+            {
+                listem.Add(veri);
+            }
+            
+            //veriler db'den cekilir.
+            System.Threading.Thread.Sleep(3000);  //3 sn beklettik.
+            return PartialView("_DataItemPartial", listem);
+        }
+
+        //public PartialViewResult VerileriGetir(string veri)
+        //{
+        //    listem.Add(veri);
+        //    //veriler db'den cekilir.
+        //    System.Threading.Thread.Sleep(3000);  //3 sn beklettik.
+        //    return PartialView("_DataItemPartial", listem);
+        //}
+    }
+}
